c0c9a0866fde9224069ebb97aff84e440f9336d6
pradhanc4 <44122332+pradhanc4@users.noreply.github.com>
2022-03-07 00:58:41 +0530

    Stop sign dome now

--- MainProgram/Final.cpp
+++ MainProgram/Final.cpp
@@ -23,13 +23,17 @@ stringstream ss;
 vector<int> histrogramLane;
 vector<int> histrogramReverse;
 
-Point2f Source[] = {Point2f(50,155), Point2f(325,155), Point2f(30,200), Point2f(334,200)};
-Point2f Destination[] = {Point2f(100,0), Point2f(280,0), Point2f(100,240), Point2f(280,240)};
+Point2f Source[] = {Point2f(35,175),Point2f(320,175),Point2f(45,195), Point2f(334,195)};
+Point2f Destination[] = {Point2f(100,0),Point2f(280,0),Point2f(100,240), Point2f(280,240)};
+
+//Point2f Source[] = {Point2f(35,155), Point2f(325,155), Point2f(30,200), Point2f(334,200)};
+//Point2f Destination[] = {Point2f(100,0), Point2f(280,0), Point2f(100,240), Point2f(280,240)};
 
 // Machine Learning Variables 
-CascadeClassifier Stop_Cascade;
-Mat frame_Stop;
-vector<Rect> Stop;
+CascadeClassifier Stop_Cascade, Object_Cascade, Traffic_Cascade;
+Mat frame_Stop,frame_Stopnew, RoI_Stop, gray_Stop, frame_Object, RoI_Object, gray_Object, frame_Traffic, RoI_Traffic, gray_Traffic,RoI_Stop_sign;
+vector<Rect> Stop, Object, Traffic;
+int dist_Stop, dist_Object, dist_Traffic;
 
  void Setup ( int argc,char **argv, RaspiCam_Cv &Camera )
   {
@@ -44,10 +48,10 @@ vector<Rect> Stop;
 }
 void Perspective()
 {
-    line(frame,Source[0], Source[1], Scalar(0,0,255), 2);
-    line(frame,Source[1], Source[3], Scalar(0,0,255), 2);
-    line(frame,Source[3], Source[2], Scalar(0,0,255), 2);
-    line(frame,Source[2], Source[0], Scalar(0,0,255), 2); 
+    line(frame,Source[0], Source[1], Scalar(0,300,0), 2);
+    line(frame,Source[1], Source[3], Scalar(0,300,0), 2);
+    line(frame,Source[3], Source[2], Scalar(0,300,0), 2);
+    line(frame,Source[2], Source[0], Scalar(0,300,0), 2); 
 
     
 
@@ -62,7 +66,9 @@ void capture()
 {
     Camera.grab();
     Camera.retrieve( frame);
+    cvtColor(frame, frame_Stop,COLOR_RGB2BGR);
     cvtColor(frame, frame, COLOR_BGR2RGB);
+    
 }
 
 void Threshold()
@@ -120,7 +126,7 @@ void LaneFinder()
     {
                         
                     LaneCenter = (RightLanePos-LeftLanePos)/2 +LeftLanePos;
-                    frameCenter = 189;
+                    frameCenter = 200;
                     
                     line(frameFinal, Point2f(LaneCenter,0), Point2f(LaneCenter,240), Scalar(0,255,0), 3);
                     line(frameFinal, Point2f(frameCenter,0), Point2f(frameCenter,240), Scalar(255,0,0), 3);
@@ -128,14 +134,38 @@ void LaneFinder()
                     Result = LaneCenter-frameCenter;
                 }    
                 
-                
+          
     void Stop_detection()
                 {
-                    if(!Stop_Cascade.load("//home/pi/Desktop/Machine_Learning//Stop_cascade.xml"))
+                    if(!Stop_Cascade.load("/home/pi/Desktop/Machine_Learning //Stop_cascade.xml"))
                     {
                   printf("Unable to open stop cascade file");
                     }
-                }
+                    RoI_Stop = frame_Stop(Rect(0,0,400,240));//200,0,200,140
+                    cvtColor(RoI_Stop, gray_Stop, COLOR_RGB2GRAY);
+                    equalizeHist(gray_Stop, gray_Stop);
+                    Stop_Cascade.detectMultiScale(gray_Stop, Stop);
+                    dist_Stop = 0;
+                  
+                 
+                    for(int i=0; i<Stop.size(); i++)
+                    {
+                          
+                  Point P1(Stop[i].x, Stop[i].y);
+                  Point P2(Stop[i].x + Stop[i].width, Stop[i].x + Stop[i].height);
+                  
+                  rectangle(RoI_Stop, P1, P2, Scalar(0, 255, 0), 2);
+                  putText(RoI_Stop, "Stop Sign", P1, FONT_HERSHEY_PLAIN, 1,  Scalar(0, 0, 255, 255), 2);
+             
+                 // std::cout<P2.x-P1.x<std::endl;
+                  //dist_Stop = (-2)*(P2.x-P1.x) + 166;//[][][]
+                  ss.str(" ");
+                  ss.clear();
+                  ss<<"Distance = "<<P2.x-P1.x<<" (Pixels)";
+                  putText(RoI_Stop, ss.str(), Point2f(1,130), 0, 1, Scalar(0,0,255), 2);	
+                  
+                    }    
+               } 
 
 int main(int argc,char **argv)
 {
@@ -175,19 +205,26 @@ int main(int argc,char **argv)
     
     
     namedWindow("ORIGINAL",WINDOW_KEEPRATIO);
-    moveWindow("ORIGINAL",0,100);
-    resizeWindow("ORIGINAL",480,480);
+    moveWindow("ORIGINAL",0,200);
+    resizeWindow("ORIGINAL",240,240);
     imshow("ORIGINAL", frame);
     
     namedWindow("Perspective",WINDOW_KEEPRATIO);
-    moveWindow("Perspective",640,100);
-    resizeWindow("Perspective",640,480);
+    moveWindow("Perspective",320,200);
+    resizeWindow("Perspective",240,240);
     imshow("Perspective", framePers);
     
     namedWindow("FINAL",WINDOW_KEEPRATIO);
-    moveWindow("FINAL",300,100);
-    resizeWindow("FINAL",640,480);
+    moveWindow("FINAL",650,200);
+    resizeWindow("FINAL",240,240);
     imshow("FINAL", frameFinal);
+    
+    
+    namedWindow("Stop_sign",WINDOW_KEEPRATIO);
+    moveWindow("Stop_sign",1000,200);
+    resizeWindow("Stop_sign",240,240);
+    imshow("Stop_sign", RoI_Stop);
+
     
     waitKey(1);
     
